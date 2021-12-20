c2bea0c32795079bb11807e8dd3886e9ca19f08f
Alex Freitas <afreitas@brasilcash.com.br>
2021-12-20 17:11:13 -0300

    inicial

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Calculadora de Magic Number Fundo Imobiliário</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top">Magic Number</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" target="_blank" href="https://instagram.com/noguedev">Follow me</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-5">Calcular Magic Number</h1>
+
+                <div class="row col-12">
+                    <div class="col-md-4">
+                        <label for="ativo">Preço do Ativo</label>
+                        <input type="text" class="form-control money" id="ativo" placeholder="Ex: R$ 104,80">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="ativo">Valor do Último Dividendo</label>
+                        <input type="text" class="form-control money" id="dividendo" placeholder="Ex: R$ 0,85">
+                        
+                    </div>
+                    <div class="col-md-4">
+                        <button class="btn btn-success  btn-lg btn-block mt-3 btn-calc" style="width: 100%;">Calcular</button>
+                    </div>
+                </div>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0" id="show" style="display: none;">O Magic Number é: <span id="num-cotas" class="text-dark">000</span> cotas/ações</p>
+            </div>
+        </header>
+        <!-- Portfolio Section-->
+        <!-- <section class="page-section portfolio" id="portfolio">
+            <div class="container">
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img class="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+        <!-- About Section-->
+        <section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">Sobre</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-6 mx-auto"><p class="lead">O magic number é basicamente o momento em que você possui uma quantidade de ativos que te geram renda passiva suficiente para que somente com os rendimentos recebidos você consiga comprar uma nova cota desse ativo e assim gerar um ciclo de aumento de capital.</p></div>
+                </div>
+                <!-- About Section Button-->
+            </div>
+        </section>
+        <!-- Contact Section-->
+        <!-- Footer-->
+        <!-- <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                     <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Location</h4>
+                        <p class="lead mb-0">
+                            2215 John Daniel Drive
+                            <br />
+                            Clark, MO 65243
+                        </p>
+                    </div>
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">About Freelancer</h4>
+                        <p class="lead mb-0">
+                            Freelance is a free to use, MIT licensed Bootstrap theme created by
+                            <a href="http://startbootstrap.com">Start Bootstrap</a>
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer> -->
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>By: <a href="http://github.com/AllexNogue" target="_blank" rel="noopener noreferrer">Allex Nogue</a></small></div>
+        </div>
+        <!-- Portfolio Modals-->
+        <!-- Portfolio Modal 1--> <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="/js/jquery.mask.js"></script>
+        <script >
+
+            $(".money").mask('000.000.000.000.000,00', {reverse: true});
+
+            $('.btn-calc').on('click', function(){
+                $('#num-cotas').html('000');
+                $('#show').css('display','none');
+
+                if($('#ativo').val() == ''){
+                    alert("Preencha o valor do Ativo");
+                    return 1;
+                }
+                if($('#dividendo').val() == ''){
+                    alert("Preencha o valor do Dividendo");
+                    return 1;
+                }
+
+                var ativo = parseFloat($('#ativo').val().replace(",", "."));
+                var dividendo = parseFloat($('#dividendo').val().replace(",", "."));
+
+                var cotas = Math.ceil(ativo/dividendo);
+
+                if (cotas > 0){
+                    $('#num-cotas').html(cotas);
+                    $('#show').css('display','block');
+                }
+            })
+        </script>
+        
+    </body>
+</html>
